1d69198d49a4dd3fdfa6cb4ed2a8edbe6c5912ab
Alexander Schuster <alexander.schuster@web.de>
2016-02-09 20:17:42 +0100

    style: switch double quotes with single quotes

--- dev/js/general.js
+++ dev/js/general.js
@@ -2,27 +2,27 @@ const height__nav = 50;
 
 //Scroll to anchors more smoothly when clicking nav links
 function scroll(element) {
-  $("html,body").animate({scrollTop: ($(element).offset().top - height__nav)}, { duration: 1000, easing: "swing"});
+  $('html,body').animate({scrollTop: ($(element).offset().top - height__nav)}, { duration: 1000, easing: 'swing'});
 }
 
 $(document).ready(function() {
-  $(".nav__logo").click(function() {
-    const element = document.getElementById("top");
+  $('.nav__logo').click(function() {
+    const element = document.getElementById('top');
     scroll(element);
     return false;
   });
-  $(".list__link--me").click(function() {
-    const element = document.getElementById("section-me");
+  $('.list__link--me').click(function() {
+    const element = document.getElementById('section-me');
     scroll(element);
     return false;
   });
-  $(".list__link--work").click(function() {
-    const element = document.getElementById("section-work");
+  $('.list__link--work').click(function() {
+    const element = document.getElementById('section-work');
     scroll(element);
     return false;
   });
-  $(".list__link--contact").click(function() {
-    const element = document.getElementById("section-contact");
+  $('.list__link--contact').click(function() {
+    const element = document.getElementById('section-contact');
     scroll(element);
     return false;
   });
@@ -31,15 +31,15 @@ $(document).ready(function() {
 //Switch Navbar between transparent and white when scrolling to the end of the header
 $(function() {
   const vh = document.documentElement.clientHeight;
-  const headerclass = $(".nav--transparent");
+  const headerclass = $('.nav--transparent');
   $(window).scroll(function() {
     const scroll = $(window).scrollTop();
 
     if (scroll >= (vh - height__nav)) {
-      headerclass.removeClass("nav--transparent").addClass("nav--white");
+      headerclass.removeClass('nav--transparent').addClass('nav--white');
     }
     else {
-      headerclass.removeClass("nav--white").addClass("nav--transparent");
+      headerclass.removeClass('nav--white').addClass('nav--transparent');
     }
   });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,134 +1,134 @@
-const gulp = require("gulp");
-const uglify = require("gulp-uglify");
-const rename = require("gulp-rename");
-const minifyCSS = require("gulp-minify-css");
-const concat = require("gulp-concat");
-const sass = require("gulp-sass");
-const browserSync = require("browser-sync");
-const htmlmin = require("gulp-htmlmin");
-const sourcemaps = require("gulp-sourcemaps");
-const babel = require("gulp-babel");
-const autoprefixer = require("gulp-autoprefixer");
-const responsive = require("gulp-responsive");
-const plumber = require("gulp-plumber");
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const minifyCSS = require('gulp-minify-css');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const htmlmin = require('gulp-htmlmin');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const responsive = require('gulp-responsive');
+const plumber = require('gulp-plumber');
 const svgmin = require('gulp-svgmin');
 
-gulp.task("scripts", function() {
-  return gulp.src("./dev/js/**/*.js")
+gulp.task('scripts', function() {
+  return gulp.src('./dev/js/**/*.js')
     .pipe(plumber())
 		.pipe(sourcemaps.init())
 			.pipe(babel({
-				presets: ["es2015"]
+				presets: ['es2015']
 			}))
     	.pipe(uglify())
-    	.pipe(concat("app.min.js"))
-		.pipe(sourcemaps.write("."))
+    	.pipe(concat('app.min.js'))
+		.pipe(sourcemaps.write('.'))
     .pipe(plumber.stop())
-    .pipe(gulp.dest("./prod/js/"))
+    .pipe(gulp.dest('./prod/js/'))
 		.pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task("styles", function() {
-  return gulp.src("./dev/scss/**/*.scss")
+gulp.task('styles', function() {
+  return gulp.src('./dev/scss/**/*.scss')
 		.pipe(sass())
 		.pipe(autoprefixer({
-			browsers: ["last 2 versions"],
+			browsers: ['last 2 versions'],
 			cascade: false
 		}))
     .pipe(minifyCSS())
-		.pipe(concat("app.min.css"))
-    .pipe(gulp.dest("./prod/css/"))
+		.pipe(concat('app.min.css'))
+    .pipe(gulp.dest('./prod/css/'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task("html", function() {
-  return gulp.src("./dev/index.html")
+gulp.task('html', function() {
+  return gulp.src('./dev/index.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest("./prod/"))
+    .pipe(gulp.dest('./prod/'))
 		.pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task("images", function() {
-  return gulp.src("./dev/images/*.{png,jpg}")
+gulp.task('images', function() {
+  return gulp.src('./dev/images/*.{png,jpg}')
     .pipe(responsive({
-      "bg-*.jpg": [
+      'bg-*.jpg': [
         {
           width: 2000,
           quality: 50,
           rename: {
-            suffix: "2x",
-            extname: ".jpg"
+            suffix: '2x',
+            extname: '.jpg'
           }
         },
         {
           width: 1000,
           quality: 50,
           rename: {
-            suffix: "1x",
-            extname: ".jpg"
+            suffix: '1x',
+            extname: '.jpg'
           }
         }
       ],
-      "coffee-*.jpg": [
+      'coffee-*.jpg': [
         {
           width: 720,
           quality: 50,
           rename: {
-            suffix: "-large",
-            extname: ".jpg"
+            suffix: '-large',
+            extname: '.jpg'
           }
         },
         {
           width: 360,
           quality: 50,
           rename: {
-            suffix: "-small",
-            extname: ".jpg"
+            suffix: '-small',
+            extname: '.jpg'
           }
         }
       ],
-      "alex.jpg": {
+      'alex.jpg': {
         quality: 50
       }
     }))
-    .pipe(gulp.dest("./prod/images/"))
+    .pipe(gulp.dest('./prod/images/'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task("svg", function() {
-  return gulp.src("./dev/images/*.svg")
+gulp.task('svg', function() {
+  return gulp.src('./dev/images/*.svg')
       .pipe(svgmin())
-      .pipe(gulp.dest("./prod/images/"));
+      .pipe(gulp.dest('./prod/images/'));
 });
 
-gulp.task("fonts", function() {
-  return gulp.src("./dev/fonts/**/*")
-  .pipe(gulp.dest("./prod/fonts"))
+gulp.task('fonts', function() {
+  return gulp.src('./dev/fonts/**/*')
+  .pipe(gulp.dest('./prod/fonts'))
 });
 
-gulp.task("browserSync", function() {
+gulp.task('browserSync', function() {
   browserSync({
     port: 3030,
     server: {
-      baseDir: "./prod"
+      baseDir: './prod'
     },
 		notify: false
   });
 });
 
-gulp.task("watch", ["browserSync", "scripts", "styles", "html", "images", "svg"], function() {
-  gulp.watch("dev/js/**/*.js", ["scripts"]);
-  gulp.watch("dev/scss/**/*.scss", ["styles"]);
-  gulp.watch("dev/index.html", ["html"]);
-	gulp.watch("dev/images/**/*", ["images", "svg"]);
+gulp.task('watch', ['browserSync', 'scripts', 'styles', 'html', 'images', 'svg'], function() {
+  gulp.watch('dev/js/**/*.js', ['scripts']);
+  gulp.watch('dev/scss/**/*.scss', ['styles']);
+  gulp.watch('dev/index.html', ['html']);
+	gulp.watch('dev/images/**/*', ['images', 'svg']);
 });
 
-gulp.task("default", ["watch"]);
+gulp.task('default', ['watch']);
